7df9e52b3fd1dff3c2d87c0a63950085c3e76566
kwaknayoung <rhkrskdud1@handong.ac.kr>
2024-11-23 22:23:41 +0900

    .

--- src/components/CreatePage.js
+++ src/components/CreatePage.js
@@ -1,5 +1,5 @@
-import React, { useState, useRef } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect, useRef } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 
 function CreatePage({ onCreate }) {
     const [formData, setFormData] = useState({
